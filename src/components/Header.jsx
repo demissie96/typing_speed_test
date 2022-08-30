db76b2a454c2bd6c2d2659b4b08582aece34b5f6
Johannes Demissie <demissie.johannes@gmail.com>
2022-08-30 22:57:09 +0200

    Initial setup and create header with unique font.

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1
+        style={{
+          textAlign: "center",
+          padding: "20px",
+          background: "yellow",
+          margin: "0",
+          fontFamily: "Dancing Script, cursive",
+        }}
+      >
+        Typing Speed Test
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
